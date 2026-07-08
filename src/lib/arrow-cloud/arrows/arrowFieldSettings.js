96e968472dc3fd332e009f162600a691a9c12ea1
Adam Trickett <me@ariom.id.au>
2026-07-08 08:33:27 +0800

    remove arrow cloud files to re-import as submodule

--- src/lib/arrow-cloud/arrows/arrowFieldSettings.js
+++
@@ -1,9 +0,0 @@
-export const arrowFieldSettings = {
-    bodyWidth: 0.4,
-    bodyDepth: 0.1,
-    headLength: 0.8,
-    headWidth: 0.9,
-    cornerRadius: 0.35,
-    cornerSteps: 14,
-    cornerOverlap: 0.04,
-};
